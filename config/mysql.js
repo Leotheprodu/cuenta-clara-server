987606e804401bb4b027ad4997a805747fc5bf0d
Leonardo <leovpc@gmail.com>
2023-10-15 04:29:40 -0600

    changed credentials of mysql connection for raiway deploy

--- config/mysql.js
+++ config/mysql.js
@@ -1,13 +1,10 @@
 const { Sequelize } = require('sequelize');
+const { port, host, user, password, database } = require('./credentials');
 
-const database = process.env.DB_NAME;
-const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const host = 'localhost';
-
-const sequelize = new Sequelize(database, username, password, {
+const sequelize = new Sequelize(database, user, password, {
     host,
     dialect: 'mysql',
+    port: port,
 });
 
 const dbConnectMySql = async () => {
