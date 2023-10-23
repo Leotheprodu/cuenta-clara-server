d728c9084456b5950db9fe3d2a27e923f3f9329c
Leonardo <leovpc@gmail.com>
2023-10-23 03:27:58 -0600

    added logic for select default business

--- controllers/users_business.js
+++ controllers/users_business.js
@@ -1,6 +1,7 @@
 const { users_businessModel } = require('../models');
 const { handleHttpError } = require('../utils/handleError');
 const { resOkData } = require('../utils/handleOkResponses');
+const { matchedData } = require('express-validator');
 
 const businessByUserCtrl = async (req, res) => {
     try {
@@ -18,6 +19,51 @@ const businessByUserCtrl = async (req, res) => {
         handleHttpError(res, 'Error al buscar los negocios del usuario');
     }
 };
+const favoriteBusinessCtrl = async (req, res) => {
+    const { id } = matchedData(req);
+    try {
+        const user_id = req.session.user.id;
+        let business = await users_businessModel.findAll({
+            where: { user_id },
+        });
+
+        if (!business) {
+            handleHttpError(res, 'El usuario no tiene negocios', 404);
+            return;
+        }
+
+        // Encontrar el negocio con default === true
+        const defaultBusiness = await oldDefaultBusiness(business);
+        // Si se encontró un negocio con default === true, actualizarlo a false
+        if (defaultBusiness) {
+            await defaultBusiness.update({ default: false });
+        }
+        console.log(defaultBusiness);
+
+        // Encontrar el negocio correspondiente al ID de la solicitud
+        const selectedBusiness = await businessSelected(business, id);
+
+        // Si se encontró un negocio con el ID de la solicitud, establecerlo como predeterminado
+        if (selectedBusiness) {
+            await selectedBusiness.update({ default: true });
+        }
+
+        // Recuperar la lista actualizada de negocios
+        business = await users_businessModel.findAll({ where: { user_id } });
+
+        resOkData(res, business);
+    } catch (error) {
+        console.error(error);
+        handleHttpError(res, 'Error al seleccionar negocio');
+    }
+};
+const businessSelected = async (business, id) => {
+    return await business.find((item) => item.id == id);
+};
+const oldDefaultBusiness = async (business) => {
+    return await business.find((item) => item.default === true);
+};
 module.exports = {
     businessByUserCtrl,
+    favoriteBusinessCtrl,
 };
--- routes/users_business.js
+++ routes/users_business.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { businessByUserCtrl } = require('../controllers/users_business');
+const {
+    businessByUserCtrl,
+    favoriteBusinessCtrl,
+} = require('../controllers/users_business');
 const { isLoggedInTrue } = require('../middleware/isLoggedIn');
+const { validatorGetBussiness } = require('../validators/business');
 
 router.get('/', isLoggedInTrue, businessByUserCtrl);
+router.get(
+    '/favorite/:id',
+    isLoggedInTrue,
+    validatorGetBussiness,
+    favoriteBusinessCtrl,
+);
 
 module.exports = router;
